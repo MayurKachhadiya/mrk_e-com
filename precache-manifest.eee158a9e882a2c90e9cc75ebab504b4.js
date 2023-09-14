4be36826a82910e1f9b736ba1f6ae75b869afbb6
Mayur Kachhadiya <mayurkachhadiya373@gmail.com>
2023-09-14 18:17:33 +0530

    Updates

---
+++ precache-manifest.eee158a9e882a2c90e9cc75ebab504b4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c9aa305fec5de79f6bb",
+    "url": "/mrk_e-com/static/css/main.3abaa69e.chunk.css"
+  },
+  {
+    "revision": "9c9aa305fec5de79f6bb",
+    "url": "/mrk_e-com/static/js/main.a525b0b1.chunk.js"
+  },
+  {
+    "revision": "28a6a4b5ef094dd2ffde",
+    "url": "/mrk_e-com/static/js/runtime~main.12da4c56.js"
+  },
+  {
+    "revision": "ad52a8c2ddb51d5472ac",
+    "url": "/mrk_e-com/static/js/2.57a17832.chunk.js"
+  },
+  {
+    "revision": "e897de3d059808493f9f",
+    "url": "/mrk_e-com/static/js/3.5aaf3fca.chunk.js"
+  },
+  {
+    "revision": "a56effccdb31215be2405af923623ecc",
+    "url": "/mrk_e-com/static/media/hero.a56effcc.jpg"
+  },
+  {
+    "revision": "2dcf183b45bbd05e14a652708380f803",
+    "url": "/mrk_e-com/static/media/logo.2dcf183b.png"
+  },
+  {
+    "revision": "4913bed7bec81ca28686c61d7aa634e6",
+    "url": "/mrk_e-com/index.html"
+  }
+];
